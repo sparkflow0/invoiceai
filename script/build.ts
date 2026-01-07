251c20235a460362e36a97f0cd730c4753b1fbc8
sparkflow0 <sparkflow2030@gmail.com>
2026-01-07 19:47:21 +0300

    'new'

--- script/build.ts
+++ script/build.ts
@@ -1,6 +1,7 @@
 import { build as esbuild } from "esbuild";
 import { build as viteBuild, createLogger } from "vite";
 import { rm, readFile } from "fs/promises";
+// server deps to bundle to reduce openat(2) syscalls
 
 // server deps to bundle to reduce openat(2) syscalls
 // which helps cold start times
